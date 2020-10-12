b00bf72beab05c9a5107d96e580fc8a62d58c86f
alvarovalerocom <72454968+alvarovalerocom@users.noreply.github.com>
2020-10-12 14:12:48 +0200

    Commit
    Random commit

--- Hero.py
+++ Hero.py
@@ -4,3 +4,6 @@ class Hero(Character):
     def __init__(self,name,hp,mp,weapon, ultimate):
         super().__init__(name,hp,mp,weapon)
         self.ultimate = ultimate
+
+    def getInfo(self):
+        print(self.name, self.hp)
--- kingdom.py
+++ kingdom.py
@@ -1,10 +1,38 @@
-
+#game logic
 from Hero import Hero
 from Weapon import BasicSword
 
 valkiria = Hero("Valkiria", 100, 20, "weapon","rage")
 print (valkiria)
-
+print (valkiria.weapon, valkiria.getInfo)
 
 basicsword = BasicSword("basic sword", 20, 20)
 print(basicsword)
+#################################################
+
+#Window
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Alv Kingdom'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
+#################################################
