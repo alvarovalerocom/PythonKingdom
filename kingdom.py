68afaf3105584723cff96ee32423e359566f8587
alvarovalerocom <72454968+alvarovalerocom@users.noreply.github.com>
2020-10-12 21:51:06 +0200

    Window fixed size, first drawn line.

--- kingdom.py
+++ kingdom.py
@@ -6,17 +6,16 @@ basicsword = BasicSword("", 20, 20,5)
 lightstick = LightStick("",3,0,100)
 print(basicsword.name)
 valkiria = Hero("Valkiria", 100, 20, basicsword,"rage")
-print(valkiria.getInfo())
+a = (valkiria.name)
 lux = Healer("lux",60,60,lightstick,"heal")
 
 #################################################
-
-#Window
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel 
-from PyQt5 import QtGui 
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtGui import QIcon
-
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
 class App(QWidget):
 
     def __init__(self):
@@ -27,11 +26,20 @@ class App(QWidget):
         self.width = 1024 
         self.height = 768
         self.initUI()
-        self.label = QLabel('This is label', self)
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QtCore.Qt.black)
+        painter.setBrush(QtCore.Qt.white)
+        painter.drawLine(1100,600,00,600)
+
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        
+        #self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedSize(self.width,self.height) 
+        self.label = QLabel(a,self)
         self.show()
     
 if __name__ == '__main__':
